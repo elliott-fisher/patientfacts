da39096649ff905c4929f692cf569d13c59b2d5e
vector-client-token <>
2022-06-05 19:11:27 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,13 @@
 
 
 @transform_pandas(
-    Output(rid="ri.vector.main.execute.5bc8561c-a56d-43bc-b038-0cf067c09e6a"),
+    Output(rid="ri.foundry.main.dataset.57d6f26d-f01a-454d-bb1c-93408d9fdd51"),
     ALL_COVID_POS_PERSONS=Input(rid="ri.foundry.main.dataset.d0f01e74-1ebb-46a5-b077-11864f9dd903")
 )
-def unnamed(ALL_COVID_POS_PERSONS):
+def patient_sample(ALL_COVID_POS_PERSONS):
+
+    proportion_of_patients_to_use = .001
+
+    return = ALL_COVID_POS_PERSONS.sample(False, proportion_of_patients_to_use, 111)
     
 
