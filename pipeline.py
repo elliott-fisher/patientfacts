17d4ec9e1001b99355ea4bf82c707136f65d64d1
vector-client-token <>
2022-06-05 19:08:38 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,9 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.5bc8561c-a56d-43bc-b038-0cf067c09e6a"),
+    ALL_COVID_POS_PERSONS=Input(rid="ri.foundry.main.dataset.d0f01e74-1ebb-46a5-b077-11864f9dd903")
+)
+def unnamed(ALL_COVID_POS_PERSONS):
+    
+
