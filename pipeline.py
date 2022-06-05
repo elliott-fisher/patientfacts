99f46fe15183112ca644be6993c4522d45aafc92
vector-client-token <>
2022-06-05 20:01:55 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,20 +1,34 @@
 
 
 @transform_pandas(
+    Output(rid="ri.foundry.main.dataset.628bfd8f-3d3c-4afb-b840-0daf4c07ac55"),
+    covid_pos_sample=Input(rid="ri.foundry.main.dataset.57d6f26d-f01a-454d-bb1c-93408d9fdd51"),
+    location=Input(rid="ri.foundry.main.dataset.efac41e8-cc64-49bf-9007-d7e22a088318"),
+    manifest=Input(rid="ri.foundry.main.dataset.b1e99f7f-5dcd-4503-985a-bbb28edc8f6f"),
+    person_lds=Input(rid="ri.foundry.main.dataset.50cae11a-4afb-457d-99d4-55b4bc2cbe66")
+)
+def covid_pos_person(covid_pos_sample, location, manifest, person_lds):
+    person = person_lds
+    
+    df = (
+        covid_pos_sample
+            .join(person_lds
+                    .select('person_id','year_of_birth','month_of_birth','day_of_birth','ethnicity_concept_name',
+                            'race_concept_name','gender_concept_name','location_id','data_partner_id'),
+                    covid_pos_sample.person_id == person_lds.person_id,
+                    how = "left"
+            )
+
+    return df            
+
+@transform_pandas(
     Output(rid="ri.foundry.main.dataset.57d6f26d-f01a-454d-bb1c-93408d9fdd51"),
     ALL_COVID_POS_PERSONS=Input(rid="ri.foundry.main.dataset.d0f01e74-1ebb-46a5-b077-11864f9dd903")
 )
-def patient_sample(ALL_COVID_POS_PERSONS):
+def covid_pos_sample(ALL_COVID_POS_PERSONS):
 
     proportion_of_patients_to_use = .001
 
     return ALL_COVID_POS_PERSONS.sample(False, proportion_of_patients_to_use, 111)
-    
-
-@transform_pandas(
-    Output(rid="ri.vector.main.execute.a580f7c7-958c-4e39-9a1f-02794e3de269"),
-    patient_sample=Input(rid="ri.foundry.main.dataset.57d6f26d-f01a-454d-bb1c-93408d9fdd51")
-)
-def unnamed(patient_sample):
     
 
