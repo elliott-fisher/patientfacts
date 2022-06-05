073aafb83548cabfacbe8aa0b5c81bd82460197e
vector-client-token <>
2022-06-05 20:32:35 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -12,10 +12,22 @@ def covid_pos_person(covid_pos_sample, location, manifest, person_lds):
     
     df = (
         covid_pos_sample.join(
-                person_lds.select('person_id','year_of_birth','month_of_birth','day_of_birth','ethnicity_concept_name','race_concept_name','gender_concept_name','location_id','data_partner_id'),
-                covid_pos_sample.person_id == person_lds.person_id,
-                how = "left"
-            ).drop(person_lds.person_id) 
+            person_lds.select(  'person_id','year_of_birth','month_of_birth','day_of_birth',
+                                'ethnicity_concept_name','race_concept_name','gender_concept_name',
+                                'location_id','data_partner_id'),
+            covid_pos_sample.person_id == person_lds.person_id,
+            how = "left"
+        ).drop(person_lds.person_id) 
+    )
+
+    df = (
+        df.join(
+            location.select('location_id','city','state','zip','county'),
+            df.location_id == location.location_id,
+            how = "left"    
+        ).drop(df.location_id)
+# location: 'location_id','city','state','zip','county'
+# manaifest data_partner_id','run_date','cdm_name','cdm_version','shift_date_yn','max_num_shift_days
     )
     return df            
 
