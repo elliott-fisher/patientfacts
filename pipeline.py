2a93aacedff382a698183f2fdc8768fb0eb3b536
vector-client-token <>
2022-06-06 16:07:19 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -68,13 +68,17 @@ def trans_covid_pos_person(covid_pos_person):
         - month_of_birth
         - day_of_birth
     """
-    df = (
+    cpp_date_columns_df = (
         covid_pos_person
             .withColumn("new_year_of_birth",  
                         F.when(covid_pos_person.year_of_birth.isNull(),1)
                         .otherwise(covid_pos_person.year_of_birth))
-            .withColumn("new_month_of_birth", F.when(covid_pos_person.month_of_birth.isNull(),1).otherwise(covid_pos_person.month_of_birth))
-            .withColumn("new_day_of_birth", F.when(covid_pos_person.day_of_birth.isNull(),1).otherwise(covid_pos_person.day_of_birth))   
+            .withColumn("new_month_of_birth", 
+                        F.when(covid_pos_person.month_of_birth.isNull(),1)
+                        .otherwise(covid_pos_person.month_of_birth))
+            .withColumn("new_day_of_birth", 
+                        F.when(covid_pos_person.day_of_birth.isNull(),1)
+                        .otherwise(covid_pos_person.day_of_birth))   
     )
     
      
