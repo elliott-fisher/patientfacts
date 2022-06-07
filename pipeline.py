6c8434aae5bed9f3c63130e5be675dee4234e6c5
vector-client-token <>
2022-06-07 18:07:08 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -159,7 +159,7 @@ def trans_covid_pos_person(covid_pos_person):
     cpp_zip_df = ( 
         cpp_race_df
             .withColumn("zip_code",
-                F.when(F.length(F.col("zip")) >  5, F.substring(F.col("zip"), 1,5))
+                F.when(F.length(F.col("zip")) >  5, F.col('zip').substr(1,5))
                # .when( F.length(F.col("zip")) <  5, "UNKNOWN")
                # .otherwise("UNKNOWN") 
             )
