baee1781465772a0cd926d85cfe59675b4ba2036
vector-client-token <>
2022-06-07 14:36:21 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -85,8 +85,8 @@ def trans_covid_pos_person(covid_pos_person):
     # Gender
     cpp_gender_df = (
         with_dob_age_df
-            .withColumn("gender_concept_name2",   F.when(F.upper(with_dob_age_df.gender_concept_name) == "MALE", "MALE")
-                                                .F.when(F.upper(with_dob_age_df.gender_concept_name) == "FEMALE", "FEMALE")
+            .withColumn("gender_concept_name",   F.when(F.upper(with_dob_age_df.gender_concept_name) == "MALE", "MALE")
+                                                 .when(F.upper(with_dob_age_df.gender_concept_name) == "FEMALE", "FEMALE")
                                                 .otherwise("UNKNOWN")
             )
     )
