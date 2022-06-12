43b09b8c896162de1987a7c3b7efff698d8c6766
vector-client-token <>
2022-06-12 20:50:22 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -411,7 +411,7 @@ def unnamed(comorbidity_by_patient):
 
     df = (
         comorbidity_by_patient
-            .withColumn("result" ,max(comorbidity_by_patient[colx] for colx in comorbidity_by_patient.drop('person_id').columns)) 
+            .withColumn("result" ,sum(comorbidity_by_patient[colx] for colx in comorbidity_by_patient.drop('person_id').columns)) 
     )
     return df
 
