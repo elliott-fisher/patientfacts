4b82530b593ef01945ecb2d11dff77a09ca1e4da
vector-client-token <>
2022-06-21 13:22:35 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -305,7 +305,7 @@ def covid_pos_sample(ALL_COVID_POS_PATIENTS):
 )
 def pf_sample( COVID_POS_PERSON_FACT):
 
-    proportion_of_patients_to_use = .001
+    proportion_of_patients_to_use = .01
 
     return COVID_POS_PERSON_FACT.sample(False, proportion_of_patients_to_use, 111)
     
@@ -349,7 +349,7 @@ def pf_visits(pf_sample, microvisit_to_macrovisit_lds, our_concept_sets, concept
     df_ED = (
         pf_visits_df
             .where(pf_visits_df.macrovisit_start_date.isNull() & (pf_visits_df.visit_concept_id.isin(ed_concept_ids)))
-            .withColumn('lab_minus_ED_visit_start_date', F.datediff('first_poslab_or_diagnosis_date','visit_start_date'))    
+            .withColumn('num_days_covid_date_ed_start_date', F.datediff('first_poslab_or_diagnosis_date','visit_start_date'))    
     )
     
     return df_ED
