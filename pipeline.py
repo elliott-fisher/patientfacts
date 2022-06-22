26aefa8a535c308fc49c4bc8974d4c6492de5471
vector-client-token <>
2022-06-22 16:56:01 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -683,6 +683,13 @@ def pf_visits_sample( microvisit_to_macrovisit_lds, our_concept_sets, concept_se
     
 
 @transform_pandas(
+    Output(rid="ri.vector.main.execute.4b66f55a-a40e-4984-9eb9-a044429db390"),
+    microvisit_to_macrovisit_lds=Input(rid="ri.foundry.main.dataset.5af2c604-51e0-4afa-b1ae-1e5fa2f4b905")
+)
+def unnamed(microvisit_to_macrovisit_lds):
+    
+
+@transform_pandas(
     Output(rid="ri.foundry.main.dataset.203392f0-b875-453c-88c5-77ca5223739e"),
     concept_set_members=Input(rid="ri.foundry.main.dataset.e670c5ad-42ca-46a2-ae55-e917e3e161b6"),
     condition_occurrence=Input(rid="ri.foundry.main.dataset.900fa2ad-87ea-4285-be30-c6b5bab60e86"),
