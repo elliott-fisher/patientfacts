379e197c669ccfdab46dc5efe5c1fff08582f80b
vector-client-token <>
2022-06-27 19:34:50 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -518,9 +518,7 @@ def pf_covid_visits( microvisit_to_macrovisit_lds, our_concept_sets, concept_set
             hosp_df
             .withColumn('macrovisit_id', F.first('macrovisit_id').over(w))
             .withColumn('first_COVID_hospitalization_start_date', F.first('covid_hospitalization_start_date').over(w))
-            .withColumn('first_COVID_hospitalization_end_date', F.first('covid_hospitalization_end_date').over(w))
-            .dropDuplicates()  
-
+            .withColumn('first_COVID_hospitalization_end_date',   F.first('covid_hospitalization_end_date').over(w))
         )
         """
         visits_df = (
